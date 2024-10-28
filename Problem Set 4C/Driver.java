4f5a9f197b21e43cd64cb76efcab6b49ef08c36d
lailawilcox <1288273@learn.vsb.bc.ca>
2024-10-28 13:35:23 -0700

    Add files via upload

---
+++ Problem Set 4C/Driver.java
@@ -0,0 +1,20 @@
+/**
+ * AP Computer Science
+ * Laila Wilcox
+ * October 28, 2024
+ * Promblem Set 4C (Driver)
+ */
+
+import java.util.Scanner;
+
+class Driver {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int x=0;
+        while(x==0){
+        
+        System.out.println(totalSentiment("SimpleReview.txt"));
+        
+    }
+    }
+}
