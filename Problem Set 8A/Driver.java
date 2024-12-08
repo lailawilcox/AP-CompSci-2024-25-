09189e3441bf2fdcf3bb974117d8cebf504f2fc0
lailawilcox <1288273@learn.vsb.bc.ca>
2024-12-08 13:43:11 -0800

    Add files via upload

--- Problem Set 8A/Driver.java
+++ Problem Set 8A/Driver.java
@@ -8,34 +8,73 @@
 import java.util.Scanner;
 
 public class Driver{
-    public static void main (String[] args) {
+    public static void main (String[] args) {    
         Scanner input = new Scanner (System.in);
-
-        String [][] grid = new String[3][3]; 
-        int count = 1;
-        String player = "X";
         boolean playing = true;
-        
-        
-        TicTacToe.InitializeBoard(grid);
-        System.out.println(player + " make your move (row, colum): ");
-        
 
         while(playing){
-            if(count%2 == 0){
-                player = "O";
-            }else{
-                player = "X";
+            String[][] board = new String[3][3];
+            TicTacToe.InitializeBoard(board);
+            boolean gameFinished = false;
+            int count = 1;
+            String player = "X";
+
+            while(gameFinished == false && count <= 9){
+                System.out.println("");
+                System.out.println("Round: " + count);
+                TicTacToe.printBoard(board);
+                System.out.print(player + ", make your move (row,column): ");
+
+                String move = input.nextLine();
+                int row = TicTacToe.findFirstValue(move);
+                int col = TicTacToe.findSecondValue(move);
+
+                //loops until the move is valid, if the move is valid it skips this
+                while(TicTacToe.isValidMove(board, row, col) == false){
+                    System.out.println("Invalid move! Please try again.");
+                    move = input.nextLine();
+                    row = TicTacToe.findFirstValue(move);
+                    col = TicTacToe.findSecondValue(move);
+                }
+
+                //if the move is valid it makes the move
+                TicTacToe.makeMove(board, row, col, player);
+                count++;
+
+                //check if the player won
+                if(TicTacToe.checkWin(board, player)) {
+                    System.out.println("");
+                    System.out.println("Congratulations! Player " + player + " wins!");
+                    TicTacToe.printBoard(board);
+                    gameFinished = true;
+                }
+
+                //checks for a draw (if the board is full)
+                if (TicTacToe.checkDraw(board)) {
+                    System.out.println("");
+                    System.out.println("It's a draw!");
+                    TicTacToe.printBoard(board);
+                    gameFinished = true;
+                }
+
+                //switches player
+                if(player.equals("X")){
+                    player = "O";
+                } else{
+                    player = "X";
+                }
             }
 
-            
-            int row = input.nextInt();
-            int col = input.nextInt();
+            //check if they want to play again
+            System.out.println("Do you want to play again? (Y/N): ");
+            String response = input.nextLine();
+            if(response.equals("Y")){
+                playing = true;
+            } else if (response.equals("N")){
+                playing = false;
+            }
             System.out.println("");
-            System.out.println("Round: " + count);
-            TicTacToe.TakeTurn(grid, row, col, player);
-            System.out.println(player + " make your move (row, colum): ");
-            count++;
         }
+        System.out.println("Thanks for playing!");
     }
 }
--- Problem Set 8A/TicTacToe.java
+++ Problem Set 8A/TicTacToe.java
@@ -8,29 +8,99 @@
 import java.util.Scanner;
 
 public class TicTacToe{
-    public static String[][] InitializeBoard(String [][] grid){
-        for(int row = 0; row < grid.length; row++){
-            for(int col = 0; col < grid[0].length; col++){
-                System.out.print(grid[row][col] = "[" + " " + "]");
+    //Fills the whole array with [ ]
+    public static String[][] InitializeBoard(String [][] board){
+        for(int row = 0; row < 3; row++){
+            for(int col = 0; col < 3; col++){
+                board[row][col] = "[ ]";
             }
-            System.out.println();
         }
-        return grid;
+        return board;
     }
 
-    public static String[][] TakeTurn(String [][] grid, int row, int col, String player){
-
-        for(int row1 = 0; row1 < grid.length; row1++){
-            for(int col1 = 0; col1 < grid[0].length; col1++){
-                if(row1 == row && col1 == col){
-                    System.out.print(grid[row1][col1] = "[" + player + "]");
-                } else{
-                     System.out.print(grid[row1][col1] = "[" + " " + "]");
-                }
+    //Prints out the board
+    public static void printBoard(String [][] board){
+        for(int row = 0; row < 3; row++){
+            for(int col = 0; col < 3; col++){
+                System.out.print(board[row][col]);
             }
             System.out.println();
         }
-        return grid;
+    }
+
+    //Checks that the move is within the boundaries of the board and not in a spot that is already taken
+    public static boolean isValidMove(String[][] board, int row, int col){
+        if(row >= 0 && row <= 2 && col >= 0 && col <= 2 && board[row][col].equals("[ ]")){
+            return true;
+        } else {
+            return false;
+        }
+    }    
+
+    //adds an X or O to the right spot on the board
+    public static void makeMove(String [][] board, int row, int col, String player){
+        board[row][col] = ("[" + player + "]");
+    }
+
+    public static boolean checkWin(String [][] board, String player){
+        String symbol = "[" + player+ "]";
+        if(checkRows(board, symbol) || checkColums(board, symbol) || checkDiagnoals(board, symbol)){
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean checkRows(String [][] board, String symbol){
+        for (int row = 0; row < 3; row++) {
+            if (board[row][0].equals(symbol) && board[row][1].equals(symbol) && board[row][2].equals(symbol)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean checkColums(String [][] board, String symbol){
+        for (int col = 0; col < 3; col++) {
+            if (board[0][col].equals(symbol) && board[1][col].equals(symbol) && board[2][col].equals(symbol)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean checkDiagnoals(String [][] board, String symbol){
+        // checks top-left to bottom-right
+        if (board[0][0].equals(symbol) && board[1][1].equals(symbol) && board[2][2].equals(symbol)) {
+            return true;
+        }
+        // checks top-right to bottom-left
+        if (board[0][2].equals(symbol) && board[1][1].equals(symbol) && board[2][0].equals(symbol)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean checkDraw(String [][] board){
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                if (board[row][col].equals("[ ]")) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static int findFirstValue(String move){
+        String firstNum = move.substring(0,1);
+        int row = Integer.parseInt(firstNum);
+        return row;
+    }
+
+    public static int findSecondValue(String move){
+        String secondNum = move.substring(2);
+        int col = Integer.parseInt(secondNum);
+        return col;
     }
 
 }
